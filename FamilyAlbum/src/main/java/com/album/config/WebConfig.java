bf0ace11c61aaa5ef1fc3ea3f7ed069afab88406
rice <chihoohoo88@gmail.com>
2025-08-07 14:24:04 +1200

    Configure interception rules

---
+++ FamilyAlbum/src/main/java/com/album/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.album.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Resource
+    private JwtInterceptor jwtInterceptor;
+
+    // Configure interception rules
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/")
+                .excludePathPatterns("/login", "/register", "/files/**", "/role/selectAll");
+    }
+}
