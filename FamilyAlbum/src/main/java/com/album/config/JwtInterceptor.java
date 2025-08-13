8050fd8dc25348bd006f6cb9d7bcfd2588315212
rice <chihoohoo88@gmail.com>
2025-08-13 15:34:56 +1200

    Allowed static resources and some non-controller requests

--- FamilyAlbum/src/main/java/com/album/config/JwtInterceptor.java
+++ FamilyAlbum/src/main/java/com/album/config/JwtInterceptor.java
@@ -17,6 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -28,6 +29,10 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        //Allowed static resources and some non-controller requests
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
         // Get token from header of Request
         String token = request.getHeader(Constants.TOKEN);
         if (token == null) {
